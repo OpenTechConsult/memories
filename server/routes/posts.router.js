76952f0f763102291522bc2d28e64d71cd7e29f4
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-10 21:22:41 +0000

    memories project
    created the post model
    created the logic to get posts and to create a post in the controller file.

--- server/routes/posts.router.js
+++ server/routes/posts.router.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { getPosts } from '../controllers/posts.controller.js'
+import { getPosts, createPost } from '../controllers/posts.controller.js'
 
 const router = express.Router()
 
 router.get('/', getPosts)
+router.post('/', createPost)
 
 export default router
